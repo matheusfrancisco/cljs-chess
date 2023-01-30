da26a15df94250d8302aaa8b2d87a732ceb99843
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-01-29 23:16:46 -0300

    add: css and add numbers_

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -17,43 +17,38 @@
      (d/div
       {:className "container"}
 
-      (d/div {:className "white"} "\u265C")
-      (d/div {:className "black"} "\u265E")
-      (d/div {:className "white"} "\u265D")
-      (d/div {:className "white"} "\u265B")
-      (d/div {:className "black"} "\u265A")
-      (d/div {:className "white"} "\u265D")
-      (d/div {:className "black"} "\u265E")
-      (d/div {:className "white"} "\u265C")
+      (d/div
+       {:className "white"}
+       (d/div {:className "number"} "8")
+       (d/span "\u265C"))
+      (d/div
+       {:className "black"}
+       (d/span "\u265E"))
+      (d/div
+       {:className "white"}
+       (d/span "\u265D"))
+      (d/div
+       {:className "black"}
+       (d/span "\u265B"))
+      (d/div {:className "white"} (d/span "\u265A"))
+      (d/div {:className "black"} (d/span "\u265D"))
+      (d/div {:className "white"} (d/span "\u265E"))
+      (d/div {:className "black"} (d/span "\u265C"))
 
-      (d/div {:className "black"} "\u265F")
-      (d/div {:className "white"} "\u265F")
-      (d/div {:className "black"} "\u265F")
-      (d/div {:className "white"} "\u265F")
-      (d/div {:className "black"} "\u265F")
-      (d/div {:className "white"} "\u265F")
-      (d/div {:className "black"} "\u265F")
-      (d/div {:className "white"} "\u265F")
+      (d/div {:className "black"}
+             (d/div {:className "number"} "7")
+             (d/span "\u265F"))
+      (d/div {:className "white"} (d/span "\u265F"))
+      (d/div {:className "black"} (d/span "\u265F"))
+      (d/div {:className "white"} (d/span "\u265F"))
+      (d/div {:className "black"} (d/span "\u265F"))
+      (d/div {:className "white"} (d/span "\u265F"))
+      (d/div {:className "black"} (d/span "\u265F"))
+      (d/div {:className "white"} (d/span "\u265F"))
 
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-      (d/div {:className "black"})
-      (d/div {:className "white"})
-
-      (d/div {:className "white"})
+      (d/div
+       {:className "white"}
+       (d/div {:className "number"} "6"))
       (d/div {:className "black"})
       (d/div {:className "white"})
       (d/div {:className "black"})
@@ -62,7 +57,8 @@
       (d/div {:className "white"})
       (d/div {:className "black"})
 
-      (d/div {:className "black"})
+      (d/div {:className "black"}
+             (d/div {:className "number"} "5"))
       (d/div {:className "white"})
       (d/div {:className "black"})
       (d/div {:className "white"})
@@ -71,23 +67,52 @@
       (d/div {:className "black"})
       (d/div {:className "white"})
 
-      (d/div {:className "white"} "\u2659")
-      (d/div {:className "black"} "\u2659")
-      (d/div {:className "white"} "\u2659")
-      (d/div {:className "black"} "\u2659")
-      (d/div {:className "white"} "\u2659")
-      (d/div {:className "black"} "\u2659")
-      (d/div {:className "white"} "\u2659")
-      (d/div {:className "black"} "\u2659")
+      (d/div {:className "white"}
+             (d/div {:className "number"} "4"))
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+      (d/div {:className "black"})
 
-      (d/div {:className "black"} "\u2656")
-      (d/div {:className "white"} "\u2658")
-      (d/div {:className "black"} "\u2657")
-      (d/div {:className "white"} "\u2655")
-      (d/div {:className "black"} "\u2654")
-      (d/div {:className "white"} "\u2657")
-      (d/div {:className "black"} "\u2658")
-      (d/div {:className "white"} "\u2656")))))
+      (d/div
+       {:className "black"}
+       (d/div {:className "number"} "3"))
+      (d/div {:className "white"})
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+      (d/div {:className "black"})
+      (d/div {:className "white"})
+
+      (d/div
+       {:className "white"}
+       (d/div {:className "number"} "2")
+       (d/span "\u2659"))
+      (d/div {:className "black"} (d/span "\u2659"))
+      (d/div {:className "white"} (d/span "\u2659"))
+      (d/div {:className "black"} (d/span "\u2659"))
+      (d/div {:className "white"} (d/span "\u2659"))
+      (d/div {:className "black"} (d/span "\u2659"))
+      (d/div {:className "white"} (d/span "\u2659"))
+      (d/div {:className "black"} (d/span "\u2659"))
+
+      (d/div {:className "black"}
+             (d/div {:className "number"} "1")
+             (d/span "\u2656")
+             (d/div {:className "letter"} "a")
+             )
+      (d/div {:className "white"} 
+             (d/span "\u2658"))
+      (d/div {:className "black"} (d/span "\u2657"))
+      (d/div {:className "white"} (d/span "\u2655"))
+      (d/div {:className "black"} (d/span "\u2654"))
+      (d/div {:className "white"} (d/span "\u2657"))
+      (d/div {:className "black"} (d/span "\u2658"))
+      (d/div {:className "white"} (d/span "\u2656"))))))
 
 ;; start your app with your React renderer
 (defn ^:export init []
