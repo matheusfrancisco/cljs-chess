89c5d9366e5d68146b97d6bf88185c5941caef75
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-02-05 10:33:37 -0300

    fix: change id calc

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -4,52 +4,24 @@
    [helix.core :refer [$]]
    [helix.dom :as d]
    [helix.hooks :as hooks]
+   [main.db :refer [state]]
    [main.lib :refer [defnc]]))
-
-(def state
-  (atom
-   {[8 0] \u265C
-    [8 1] \u265E
-    [8 2] \u265D
-    [8 3] \u265B
-    [8 4] \u265A
-    [8 5] \u265D
-    [8 6] \u265E
-    [8 7] \u265C
-    [7 0] \u265F
-    [7 1] \u265F
-    [7 2] \u265F
-    [7 3] \u265F
-    [7 4] \u265F
-    [7 5] \u265F
-    [7 6] \u265F
-    [7 7] \u265F
-;;white
-    [2 0] \u2659
-    [2 1] \u2659
-    [2 2] \u2659
-    [2 3] \u2659
-    [2 4] \u2659
-    [2 5] \u2659
-    [2 6] \u2659
-    [2 7] \u2659
-    [1 0] \u2656
-    [1 1] \u2658
-    [1 2] \u2657
-    [1 3] \u2655
-    [1 4] \u2654
-    [1 5] \u2657
-    [1 6] \u2658
-    [1 7] \u2656}))
 
 (defn classname [n]
   (if (even? n) "light" "dark"))
 
 (defn board-id [row col]
-  (str (char (+ 96 col)) row))
+  (str (char (+ 96 (inc col))) row))
 
 (defn col-index [col]
   (str (char (+ 97 col))))
+
+(defn handle-drag [e _start]
+  (js/console.log e))
+
+(defn handle-click [e clicked r c]
+  (js/console.log e)
+  (js/console.log clicked r c))
 
 (defn make-initial-board []
   (d/table
@@ -61,13 +33,20 @@
        (d/th {:key col} (col-index col))))
     (for [row (range 8 0 -1)]
       (d/tr {:key row}
-       (d/th  row)
-       (for [col (range 8)]
-         (d/td
-          {:className (classname (+ row col))
-           :key (board-id row col)
-           :id (board-id row col)}
-          (get @state [row col]))))))))
+            (d/th  row)
+            (for [col (range 8)]
+              (d/td
+               {:className (classname (+ row col))
+                :key (board-id row col)
+                :id (board-id row col)}
+               (let [{:keys [value _type]} (get @state [row col])
+                     id (board-id row col)]
+                 (d/div {:id id
+                         :draggable true
+                         :on-click #(handle-click % id row col)
+                         :on-drag #(handle-drag % id)
+                         :on-drag-over #(.preventDefault %)}
+                        value)))))))))
 
 ;; app
 (defnc app []
